0a7ef867d7e7b487c8c02900c192d3ba82dbc8d9
Thiyagarajan Veluchamy <thiyagarajannv@yahoo.com>
2009-04-03 11:53:10 +0530

    new file:   MIT-LICENSE

---
+++ init.rb
@@ -0,0 +1,2 @@
+# Include hook code here
+require 'geography_division_select'
---
+++ install.rb
@@ -0,0 +1,6 @@
+# Install hook code here
+puts "DISCLAIMER: The list of Regions, Countries, States/Provinces/Districts/Atolls/Islands, etc., are based on informatoin collected in the internet.
+We are thankful to various sites  especially to Wikipedia.org for having provided the information. All the data provided have been converted into
+pure english alphabet format and special characters are converted to its nearest english alphabet. The authors have tried their level best to provide 
+adequate and correct information based on the analysis and takes no responsiibility if so ever if any information is incorrect or is in form that 
+offend any users. The authors acknowledges if any error is brought to notice and shall make necessary changes. Please review the data carefully before you use it"
